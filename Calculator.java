1927ae5c9ab6e0d0ffa7f2b4d3b2691baea21cde
victorylum <lumtegha@email.com>
2025-03-03 16:16:43 +0200

    first commit

---
+++ Calculator.java
@@ -0,0 +1,28 @@
+class Calculator{
+int a=60;
+int b=70;
+int c=20;
+int d=30;
+int e=12;
+int f=3;
+int answer;
+public int add (){
+answer =a+b;
+return answer;
+}
+public int subtract(){
+answer =b-c;
+return answer;
+}
+public int mutiple(){
+answer =f*e;
+return answer;
+}
+public int divide(){
+answer =a/d;
+return answer;
+}
+public static void main (String[]args){
+Calculator c=new Calculator();
+  }
+}
