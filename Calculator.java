e2aa2855ebbe5899fec16abf2c4e9feb44203f73
victorylum <lumtegha@email.com>
2025-03-04 21:39:12 +0200

    first

--- Calculator.java
+++ Calculator.java
@@ -1,10 +1,10 @@
 class Calculator{
-int a=60;
-int b=70;
-int c=20;
-int d=30;
-int e=12;
-int f=3;
+static int a=60;
+static int b=70;
+static int c=20;
+static int d=30;
+static int e=12;
+static int f=3;
 int answer;
 public int add (){
 answer =a+b;
@@ -14,7 +14,7 @@ public int subtract(){
 answer =b-c;
 return answer;
 }
-public int mutiple(){
+public int multiple(){
 answer =f*e;
 return answer;
 }
@@ -24,5 +24,13 @@ return answer;
 }
 public static void main (String[]args){
 Calculator c=new Calculator();
+  int g=c.add();
+  System.out.println(g);
+  int h=c.subtract();
+  System.out.println(h);
+    int j=c.multiple();
+  System.out.println(j);
+  int k=c.divide();
+  System.out.println(k);
   }
 }
